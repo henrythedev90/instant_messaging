086b726f7e3165e9ba866f716b856df925aead55
henrythedev90 <henrythedev90@gmail.com>
2025-02-07 16:17:36 -0500

    updating code

---
+++ nextjs-api/pages/api/test.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    res.status(200).json({
+      message: "Hello from the API! This is the testing route",
+      timestamp: Date.now().toString(),
+    });
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
